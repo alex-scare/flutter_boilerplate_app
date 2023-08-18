13ac9ba7c69f1b7f39d52a5799f3201cac682847
alexscare <al.pugachev96@gmail.com>
2023-08-18 12:57:04 +0400

    add file picker

--- lib/services/file_system/file_system_service.dart
+++ lib/services/file_system/file_system_service.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
@@ -10,6 +11,7 @@ class FileSystemService {
 
   static Directory? _appSupportDirectory;
   static Directory? _appDocumentsDirectory;
+  static Directory? _appTemporaryDirectory;
 
   static Future<Directory> getSupportDirectory() async {
     return _appSupportDirectory ??= await getApplicationSupportDirectory();
@@ -17,6 +19,10 @@ class FileSystemService {
 
   static Future<Directory> getDocumentsDirectory() async {
     return _appDocumentsDirectory ??= await getApplicationDocumentsDirectory();
+  }
+
+  static Future<Directory> getAppTemporaryDirectory() async {
+    return _appTemporaryDirectory ??= await getTemporaryDirectory();
   }
 
   static Future<void> shareFile(File file) async {
@@ -38,5 +44,20 @@ class FileSystemService {
     } on PlatformException catch (e, stack) {
       _log.error('platform error: file download error', e, stack);
     }
+  }
+
+  static Future<File?> pickFile() async {
+    try {
+      final result = await FilePicker.platform.pickFiles(
+        type: FileType.any,
+      );
+      if (result != null) {
+        final file = File(result.files.single.path!);
+        return file;
+      }
+    } on PlatformException catch (e, stack) {
+      _log.error('platform error: file pick error', e, stack);
+    }
+    return null;
   }
 }
--- lib/services/isar/isar_service.dart
+++ lib/services/isar/isar_service.dart
@@ -9,8 +9,9 @@ const List<CollectionSchema> _isarSchemas = [
 ];
 
 class IsarService {
-  static const isarDirName = 'isar';
   final _log = DevLogger('isar');
+  static const _isarFileName = 'isar_db';
+  static const isarDirName = 'isar';
 
   // singleton boilerplate
   static final IsarService _singleton = IsarService._internal();
@@ -22,15 +23,48 @@ class IsarService {
 
   late Future<Isar> isar;
 
-  Future<Isar> _initIsar() async {
+  Future<Isar> _initIsar([File? file]) async {
     final dir = await FileSystemService.getDocumentsDirectory();
     final isarDir = Directory('${dir.path}/$isarDirName');
+    final isarFilePath = '${isarDir.path}/$_isarFileName.isar';
+
+    if (file != null) {
+      File(isarFilePath).writeAsBytesSync(file.readAsBytesSync());
+    }
 
     if (!isarDir.existsSync()) {
       isarDir.createSync();
     }
 
     _log.info('service started');
-    return Isar.openSync(_isarSchemas, directory: isarDir.path);
+    return Isar.openSync(
+      _isarSchemas,
+      directory: isarDir.path,
+      name: _isarFileName,
+    );
+  }
+
+  Future<void> shareBackupFile() async {
+    final dir = await FileSystemService.getAppTemporaryDirectory();
+    final file = File('${dir.path}/$_isarFileName.isar');
+
+    if (file.existsSync()) {
+      file.deleteSync();
+    }
+
+    final isar = await this.isar;
+    await isar.copyToFile(file.path);
+
+    await FileSystemService.shareFile(file);
+    file.delete();
+  }
+
+  Future<void> restoreFromFile() async {
+    final file = await FileSystemService.pickFile();
+
+    if (file == null) return;
+
+    await (await isar).close();
+    isar = _initIsar(file);
   }
 }
